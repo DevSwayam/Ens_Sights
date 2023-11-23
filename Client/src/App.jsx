3b759b89d77e7943ef3c96f73eaaba8908c98508
Ayush Singh <ayushsinghmi711@gmail.com>
2023-11-23 23:57:58 +0530

    initial commit

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,16 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline bg-green-200">
-      Hello world!
-    </h1>
-    </>
+    <div className='bg-slate-300'>
+     <Header/> 
+    </div>
   )
 }
 
---
+++ Client/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='flex  h-screen'>
+    <h1 className='font-semibold text-3xl bg-slate-800 h-[50px] mt-[10px] ml-[10px] p-[5px]
+    text-white border rounded-xl hover:text-slate-800 hover:bg-white'>
+    Ens Sights</h1>
+  </div>
+  )
+}
+
+export default Header
